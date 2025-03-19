8d5bab188fde29067096fb79a59c375a80172a64
Harsh Meshram <harshmeshram21@gmail.com>
2025-03-19 17:07:05 +0530

    all files are added

---
+++ service/auth.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+
+export const authGuard: CanActivateFn = (route, state) => {
+  debugger;
+  const router = inject(Router);
+  const isLoggedIn = !!localStorage.getItem("token");
+  if (!isLoggedIn) {
+    router.navigateByUrl("login");
+    return false;
+  }
+  return true;
+};
---
+++ service/department.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DepartmentService {
+  constructor(private http: HttpClient) {}
+
+  fetchDepartments() {
+    return this.http.get(
+      "https://projectapi.gerasim.in/api/Complaint/GetParentDepartment"
+    );
+  }
+}
+
+//********* */ What is a Service in Angular?**************
+// In Angular, a service is a class that provides shared functionality across components, such as:
+
+// Fetching data from an API
+// Managing application state
+// Handling business logic
+// Performing reusable operations (e.g., logging, authentication, data transformation)
+
+// ***********Why Use Services?***********
+// Code Reusability – Prevents code duplication by centralizing logic.
+// Separation of Concerns – Keeps components lightweight and focused only on the UI.
+// Dependency Injection (DI) – Services are injected into components, making them easier to manage and test.
